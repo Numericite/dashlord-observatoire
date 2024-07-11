b390c97fe02bf173de1ad996f42c2da65607acd8
Clément <clement@numericite.eu>
2024-07-11 16:05:35 +0200

    feat: new scan with jdma 2.0 API

--- actions/jdma/index.js
+++ actions/jdma/index.js
@@ -1,25 +1,52 @@
 const fetch = (...args) =>
-  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-const getXWikiJdmaData = (
-  id,
-  startDate,
-  endDate,
-  usernameXWiki,
-  passwordXWiki
-) => {
-  fetch(
-    `https://observatoire.numerique.gouv.fr/rest/observatoire/avistats/aggregatebyday/demarche/${id}?media=json&question=easy&question=satisfaction&question=comprehensible&date_start=${startDate}&date_end=${endDate}`,
-    {
-      headers: {
-        Authorization:
-          'Basic ' +
-          Buffer.from(`${usernameXWiki}:${passwordXWiki}`).toString('base64')
+const getXWikiJdmaData = (id, startDate, endDate, jdmaToken) => {
+  fetch(`https://jedonnemonavis.numerique.gouv.fr/api/open-api/stats`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jdmaToken}`,
+    },
+    body: JSON.stringify({
+      field_codes: ["satisfaction", "comprehension", "contact_satisfaction"],
+      product_ids: [parseInt(id)],
+      start_date: startDate,
+      end_date: endDate,
+      interval: "none",
+    }),
+  }).then((response) => {
+    response.json().then((json) => {
+      if (json.data[0]) {
+        if (json.data[0].intervals[0]) {
+          const data = json.data[0].intervals[0].data;
+          if (data) {
+            console.log(
+              data.reduce(
+                (acc, item) => {
+                  item.number_hits.forEach((nh) => {
+                    acc[item.category][nh.intention] = nh.count;
+
+                    if (item.category === "satisfaction") {
+                      acc.answersTotal += nh.count;
+                    }
+                  });
+
+                  return acc;
+                },
+                {
+                  answersTotal: 0,
+                  satisfaction: {},
+                  comprehension: {},
+                  contact_satisfaction: {},
+                  dateStart: new Date(startDate).getTime(),
+                  dateEnd: new Date(endDate).getTime(),
+                }
+              )
+            );
+          }
+        }
       }
-    }
-  ).then(response => {
-    response.json().then(json => {
-      console.log(JSON.stringify(json));
     });
   });
 };
@@ -28,7 +55,6 @@ module.exports = { getXWikiJdmaData };
 
 if (require.main === module) {
   getXWikiJdmaData(
-    process.argv[process.argv.length - 5],
     process.argv[process.argv.length - 4],
     process.argv[process.argv.length - 3],
     process.argv[process.argv.length - 2],
