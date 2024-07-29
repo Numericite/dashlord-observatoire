1cd48fed38590f431c0cb9b1b0d1c3d6768f48ca
Clément <clement@numericite.eu>
2024-07-29 10:46:15 +0200

    fix: getObservatoireStats on jdma

--- actions/jdma/index.js
+++ actions/jdma/index.js
@@ -1,51 +1,47 @@
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-const getXWikiJdmaData = (id, startDate, endDate, jdmaToken) => {
-  fetch(`https://jedonnemonavis.numerique.gouv.fr/api/open-api/stats`, {
-    method: "POST",
+const encodeQueryParams = (params) => {
+  return Object.keys(params)
+    .map((key) => {
+      const value = params[key];
+      return (
+        encodeURIComponent(key) +
+        "=" +
+        encodeURIComponent(
+          typeof value === "object" ? JSON.stringify(value) : value
+        )
+      );
+    })
+    .join("&");
+};
+
+const getXWikiJdmaData = (id, startDate, endDate) => {
+  const params = {
+    input: {
+      json: {
+        product_id: id,
+        start_date: startDate,
+        end_date: endDate,
+      },
+    },
+  };
+
+  const url = `https://jedonnemonavis.numerique.gouv.fr/api/trpc/answer.getObservatoireStats?${encodeQueryParams(
+    params
+  )}`;
+
+  fetch(url, {
+    method: "GET",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${jdmaToken}`,
     },
-    body: JSON.stringify({
-      field_codes: ["satisfaction", "comprehension", "contact_satisfaction"],
-      product_ids: [parseInt(id)],
-      start_date: startDate,
-      end_date: endDate,
-      interval: "none",
-    }),
   }).then((response) => {
     response.json().then((json) => {
-      if (json.data[0]) {
-        if (json.data[0].intervals[0]) {
-          const data = json.data[0].intervals[0].data;
-          if (data) {
-            console.log(
-              data.reduce(
-                (acc, item) => {
-                  item.number_hits.forEach((nh) => {
-                    acc[item.category][nh.intention] = nh.count;
+      const data = json.result?.data?.json;
 
-                    if (item.category === "satisfaction") {
-                      acc.answersTotal += nh.count;
-                    }
-                  });
-
-                  return acc;
-                },
-                {
-                  answersTotal: 0,
-                  satisfaction: {},
-                  comprehension: {},
-                  contact_satisfaction: {},
-                  dateStart: new Date(startDate).getTime(),
-                  dateEnd: new Date(endDate).getTime(),
-                }
-              )
-            );
-          }
-        }
+      if (data) {
+        console.log(data);
       }
     });
   });
@@ -55,7 +51,6 @@ module.exports = { getXWikiJdmaData };
 
 if (require.main === module) {
   getXWikiJdmaData(
-    process.argv[process.argv.length - 4],
     process.argv[process.argv.length - 3],
     process.argv[process.argv.length - 2],
     process.argv[process.argv.length - 1]
