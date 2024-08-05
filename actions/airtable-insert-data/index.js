3d8ab2075e1ca50008b26d20e13a4d045664600a
Clément <clement@numericite.eu>
2024-08-05 17:01:14 +0200

    feat: adapt airtable-insert-data to new jdma data

--- actions/airtable-insert-data/index.js
+++ actions/airtable-insert-data/index.js
@@ -1,30 +1,25 @@
 const fetch = (...args) =>
-  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const field_names = {
-  id: 'ID',
-  edition: 'Lien vers statistiques édition',
-  noMaj: 'MAJ manuelle de la satisfaction',
+  id: "ID",
+  edition: "Lien vers statistiques édition",
+  noMaj: "MAJ manuelle de la satisfaction",
   // a11y: '[Dashlord] - Mention accessibilité',
   // a11yLink: "[Dashlord] - Lien de la déclaration d'accessibilité",
   // rgaaTaux: '[Dashlord] - Taux RGAA',
   // rgaaDate: "[Dashlord] - Date de la déclaration d'accessibilité",
-  jdmaCount: '[Dashlord] - JDMA nombre de réponses',
-  jdmaSatisfactionCount: '[Dashlord] - JDMA satisfaction nombre de réponses',
-  jdmaSatisfactionMark: '[Dashlord] - JDMA satisfaction note',
+  jdmaCount: "[Dashlord] - JDMA nombre de réponses",
+  jdmaSatisfactionCount: "[Dashlord] - JDMA satisfaction nombre de réponses",
+  jdmaSatisfactionMark: "[Dashlord] - JDMA satisfaction note",
   // jdmaEasyCount: '[Dashlord] - JDMA facilité nombre de réponses',
   // jdmaEasyMark: '[Dashlord] - JDMA facilité note',
-  jdmaComprehensibleCount: '[Dashlord] - JDMA complexité nombre de réponses',
-  jdmaComprehensibleMark: '[Dashlord] - JDMA complexité note'
-};
-
-const getDataFromJdmaNumbers = (postive, neutral, negative) => {
-  const count = postive + neutral + negative;
-  const mark = (postive * 10 + neutral * 5) / count;
-  return {
-    count,
-    mark
-  };
+  jdmaComprehensionCount: "[Dashlord] - JDMA complexité nombre de réponses",
+  jdmaComprehensionMark: "[Dashlord] - JDMA complexité note",
+  jdmaAutonomyCount: "[Dashlord] - JDMA autonomie nombre de réponses",
+  jdmaAutonomyMark: "[Dashlord] - JDMA autonomie note",
+  jdmaContactCount: "[Dashlord] - JDMA aide nombre de réponses",
+  jdmaContactMark: "[Dashlord] - JDMA aide note",
 };
 
 const insertAirtableData = async (
@@ -51,102 +46,101 @@ const insertAirtableData = async (
   // const rgaa = JSON.parse(JSON.parse(rgaa_json).toString());
   // body.fields[field_names.rgaaTaux] = rgaa.taux ? rgaa.taux + '%' : '';
   // body.fields[field_names.rgaaDate] = rgaa.date ? rgaa.date : '';
-
   //JDMA
   const jdma = JSON.parse(JSON.parse(jdma_json).toString());
-  body.fields[field_names.jdmaCount] = jdma.answersTotal
-    ? parseInt(jdma.answersTotal)
+
+  if (!jdma.data || !jdma.metadata) {
+    process.exit();
+  }
+
+  body.fields[field_names.jdmaCount] = jdma.metadata.satisfaction_count
+    ? parseInt(jdma.metadata.satisfaction_count)
     : 0;
 
   // jdma satisfaction
   if (
-    jdma.satisfaction &&
-    jdma.satisfaction.positive !== undefined &&
-    jdma.satisfaction.neutral !== undefined &&
-    jdma.satisfaction.negative !== undefined
+    jdma.data.satisfaction !== undefined &&
+    jdma.metadata.satisfaction_count !== undefined
   ) {
-    const jdmaSatisfactionData = getDataFromJdmaNumbers(
-      parseInt(jdma.satisfaction.positive),
-      parseInt(jdma.satisfaction.neutral),
-      parseInt(jdma.satisfaction.negative)
-    );
-    body.fields[field_names.jdmaSatisfactionCount] = jdmaSatisfactionData.count;
-    body.fields[field_names.jdmaSatisfactionMark] = jdmaSatisfactionData.mark;
+    body.fields[field_names.jdmaSatisfactionCount] =
+      jdma.metadata.satisfaction_count;
+    body.fields[field_names.jdmaSatisfactionMark] = jdma.data.satisfaction;
   }
 
-  // jdma easy
-  // if (
-  //   jdma.easy &&
-  //   jdma.easy.positive &&
-  //   jdma.easy.neutral &&
-  //   jdma.easy.negative
-  // ) {
-  //   const jdmaEasyData = getDataFromJdmaNumbers(
-  //     parseInt(jdma.easy.positive),
-  //     parseInt(jdma.easy.neutral),
-  //     parseInt(jdma.easy.negative)
-  //   );
-  //   body.fields[field_names.jdmaEasyCount] = jdmaEasyData.count;
-  //   body.fields[field_names.jdmaEasyMark] = jdmaEasyData.mark;
-  // }
-
-  // jdma comprehensible
+  // jdma comprehension
   if (
-    jdma.comprehensible &&
-    jdma.comprehensible.positive !== undefined &&
-    jdma.comprehensible.neutral !== undefined &&
-    jdma.comprehensible.negative !== undefined
+    jdma.data.satisfaction !== undefined &&
+    jdma.metadata.satisfaction_count !== undefined
   ) {
-    const jdmaComprehensibleData = getDataFromJdmaNumbers(
-      parseInt(jdma.comprehensible.positive),
-      parseInt(jdma.comprehensible.neutral),
-      parseInt(jdma.comprehensible.negative)
-    );
-    body.fields[field_names.jdmaComprehensibleCount] =
-      jdmaComprehensibleData.count;
-    body.fields[field_names.jdmaComprehensibleMark] =
-      jdmaComprehensibleData.mark;
+    body.fields[field_names.jdmaComprehensionCount] =
+      jdma.metadata.comprehension_count;
+    body.fields[field_names.jdmaComprehensionMark] = jdma.data.comprehension;
   }
 
-  // console.log('body a11y mention : ', body.fields[field_names.a11y]);
-  // console.log('body a11y link : ', body.fields[field_names.a11yLink]);
-  // console.log('body rgaa taux : ', body.fields[field_names.rgaaTaux]);
-  // console.log('body rgaa date : ', body.fields[field_names.rgaaDate]);
-  console.log('body jdma count : ', body.fields[field_names.jdmaCount]);
+  // jdma autonomy
+  if (
+    jdma.data.autonomy !== undefined &&
+    jdma.metadata.autonomy_count !== undefined
+  ) {
+    body.fields[field_names.jdmaAutonomyCount] = jdma.metadata.autonomy_count;
+    body.fields[field_names.jdmaAutonomyMark] = jdma.data.autonomy;
+  }
+
+  // jdma help
+  if (
+    jdma.data.contact !== undefined &&
+    jdma.metadata.contact_count !== undefined
+  ) {
+    body.fields[field_names.jdmaContactCount] = jdma.metadata.contact_count;
+    body.fields[field_names.jdmaContactMark] = jdma.data.contact;
+  }
+
+  console.log("body jdma count : ", body.fields[field_names.jdmaCount]);
   console.log(
-    'body jdma satisfaction count : ',
+    "body jdma satisfaction count : ",
     body.fields[field_names.jdmaSatisfactionCount]
   );
   console.log(
-    'body jdma satisfaction mark : ',
+    "body jdma satisfaction mark : ",
     body.fields[field_names.jdmaSatisfactionMark]
   );
-  // console.log(
-  //   'body jdma easy count : ',
-  //   body.fields[field_names.jdmaEasyCount]
-  // );
-  // console.log('body jdma easy mark : ', body.fields[field_names.jdmaEasyMark]);
   console.log(
-    'body jdma comprehensible count : ',
-    body.fields[field_names.jdmaComprehensibleCount]
+    "body jdma comprehension count : ",
+    body.fields[field_names.jdmaComprehensionCount]
   );
   console.log(
-    'body jdma comprehensible mark : ',
-    body.fields[field_names.jdmaComprehensibleMark]
+    "body jdma comprehension mark : ",
+    body.fields[field_names.jdmaComprehensionMark]
+  );
+  console.log(
+    "body jdma autonomy count : ",
+    body.fields[field_names.jdmaAutonomyCount]
+  );
+  console.log(
+    "body jdma autonomy mark : ",
+    body.fields[field_names.jdmaAutonomyMark]
+  );
+  console.log(
+    "body jdma contact count : ",
+    body.fields[field_names.jdmaContactCount]
+  );
+  console.log(
+    "body jdma contact mark : ",
+    body.fields[field_names.jdmaContactMark]
   );
 
   let response = await fetch(
     `https://api.airtable.com/v0/${base_id}/${procedures_table_name}?${new URLSearchParams(
       {
-        filterByFormula: `AND({${field_names.id}} = "${id}", FIND('Édition actuelle', ARRAYJOIN({${field_names.edition}})))`
+        filterByFormula: `AND({${field_names.id}} = "${id}", FIND('Édition actuelle', ARRAYJOIN({${field_names.edition}})))`,
       }
     ).toString()}`,
     {
-      method: 'GET',
+      method: "GET",
       headers: {
         Authorization: `Bearer ${api_key}`,
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     }
   );
   const json = await response.json();
@@ -155,16 +149,16 @@ const insertAirtableData = async (
 
   if (record) {
     if (!record.fields[field_names.noMaj]) {
-      console.log('body', JSON.stringify(body));
+      console.log("body", JSON.stringify(body));
       const patchDemarche = await fetch(
         `https://api.airtable.com/v0/${base_id}/${procedures_table_name}/${record.id}`,
         {
-          method: 'PATCH',
+          method: "PATCH",
           headers: {
             Authorization: `Bearer ${api_key}`,
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify(body)
+          body: JSON.stringify(body),
         }
       );
       console.log(patchDemarche);
